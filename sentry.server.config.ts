49127ef49b5c8903a29841f81db430ed95954428
Matt Stone <mstone2@atlassian.com>
2025-01-13 08:23:13 +1000

    Sentry tweaks

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,11 +5,14 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://ff95aa5fc9ad89d4cf2d13167fa1df7e@o4505628890693632.ingest.us.sentry.io/4508632906268672",
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
+
+  // Only enable Sentry in production.
+  enabled: process.env.NODE_ENV === "production",
 });
